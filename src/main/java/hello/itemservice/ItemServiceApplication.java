03da518a595ae83deb3e5d8cc57585bd38265927
junyoungs7 <junyoungs7@naver.com>
2023-11-21 22:54:45 +0900

    2023-11-21: Querydsl

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -18,10 +18,9 @@ import javax.sql.DataSource;
 //@Import(JdbcTemplateV2Config.class)
 
 //@Import(JdbcTemplateV3Config.class)
-//@Import(MyBatisConfig.class)
-
+//@Import(JpaConfig.class)
 @Slf4j
-@Import(JpaConfig.class)
+@Import(SpringDataJpaConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
@@ -48,5 +47,6 @@ public class ItemServiceApplication {
 //	}
 
 }
+//@Import(MyBatisConfig.class)
 
 
