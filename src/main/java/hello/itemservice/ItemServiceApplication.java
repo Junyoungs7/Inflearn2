bc1473186e9ea637d44fc88b108bda1b055237ad
junyoungs7 <junyoungs7@naver.com>
2023-11-21 17:57:37 +0900

    2023-11-21: JPA

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -16,9 +16,12 @@ import javax.sql.DataSource;
 //@Import(MemoryConfig.class)
 //@Import(JdbcTemplateV1Config.class)
 //@Import(JdbcTemplateV2Config.class)
+
 //@Import(JdbcTemplateV3Config.class)
+//@Import(MyBatisConfig.class)
+
 @Slf4j
-@Import(MyBatisConfig.class)
+@Import(JpaConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
@@ -45,3 +48,5 @@ public class ItemServiceApplication {
 //	}
 
 }
+
+
